bbd750d8e4d916c5ad3bab4c1d693708376f9a30
syouru94 <xero120017@gmail.com>
2018-03-27 22:13:07 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'First Project',
+    packages = ['package1', 'package2' ] ,
+    version = 'v0.2',
+    description = '專案試做',
+    author = 'syouru94',
+    author_email = 'xero120017@gmail.com',
+    url = 'https://github.com/syouru94/pip_project_1',
+    download_url = 'https://github.com/syouru94/pip_project_1/archive/v0.1.zip',
+    keywords = ['test'],
+    classifiers = []
+)
